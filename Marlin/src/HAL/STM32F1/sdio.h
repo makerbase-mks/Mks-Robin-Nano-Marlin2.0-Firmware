205b83061829e6e7d77a4d0d67beb88fc46a641b
Kokorone Tsurisa <gabehahr@gmail.com>
2020-04-22 09:50:55 -0700

    Lowering SDIO clock to fix sd print issues

--- Marlin/src/HAL/STM32F1/sdio.h
+++ Marlin/src/HAL/STM32F1/sdio.h
@@ -100,7 +100,7 @@
 #define SDIO_DATA_TIMEOUT                    100U           /* Read data transfer timeout */
 #define SDIO_WRITE_TIMEOUT                   200U           /* Write data transfer timeout */
 
-#define SDIO_CLOCK                           18000000       /* 18 MHz */
+#define SDIO_CLOCK                           4500000       /* 4.5 MHz */
 
 // ------------------------
 // Types
