5c26c15cb94b85b0ea6389cfa3459f953926bfbf
Claude BURGER <chlorel@gmail.com>
2021-01-24 21:13:09 +0100

    Version.h

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2020-12-30"
+  #define STRING_DISTRIBUTION_DATE "2021-01-24"
 #endif
 
 /**
@@ -73,7 +73,7 @@
  * Define a generic printer name to be output to the LCD after booting Marlin.
  */
 #ifndef MACHINE_NAME
-  #define MACHINE_NAME "3D Printer"
+  #define MACHINE_NAME "Wanhao D12/230"
 #endif
 
 /**
