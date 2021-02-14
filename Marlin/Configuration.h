7a6b6f4acb071651a01ce1c10a69918f76c78d9f
Sergey Kakurin <sergeynk92@gmail.com>
2021-02-14 14:37:33 +0300

    Fix Z speeds

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1010,7 +1010,7 @@
 #define XY_PROBE_SPEED 7980
 
 // Feedrate (mm/m) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_SPEED_FAST (20)
+#define Z_PROBE_SPEED_FAST (4*60)
 
 // Feedrate (mm/m) for the "accurate" probe of each point
 #define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 4)
@@ -1536,7 +1536,7 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (20) }
+#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (4*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
