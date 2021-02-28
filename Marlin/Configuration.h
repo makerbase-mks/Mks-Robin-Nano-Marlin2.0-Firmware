3b36bae3b28af605a2cd7a288622a1340a2c2a9c
Sergey Kakurin <sergeynk92@gmail.com>
2021-02-28 12:48:43 +0300

    Calibrate hotend's PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -492,9 +492,9 @@
     #define DEFAULT_Ki_LIST {   0.85,   0.85 }
     #define DEFAULT_Kd_LIST { 56.55, 56.55 }
   #else
-    #define DEFAULT_Kp  15.30
-    #define DEFAULT_Ki   0.85
-    #define DEFAULT_Kd 56.55
+    #define DEFAULT_Kp 12.46
+    #define DEFAULT_Ki 0.84
+    #define DEFAULT_Kd 46.26
   #endif
 #endif // PIDTEMP
 
@@ -544,7 +544,7 @@
   //#define PID_DEBUG             // Sends debug data to the serial port. Use 'M303 D' to toggle activation.
   //#define PID_OPENLOOP          // Puts PID in open loop. M104/M140 sets the output power from 0 to PID_MAX
   //#define SLOW_PWM_HEATERS      // PWM with very low frequency (roughly 0.125Hz=8s) and minimum state time of approximately 1s useful for heaters driven by a relay
-  #define PID_FUNCTIONAL_RANGE 10 // If the temperature difference between the target temperature and the actual temperature
+  #define PID_FUNCTIONAL_RANGE 30 // If the temperature difference between the target temperature and the actual temperature
                                   // is more than PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
 #endif
 
