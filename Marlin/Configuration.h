688fd5ed58d779d7a1d0c3529f642b9d8e872e94
Davide Zavattero <davide@davidezavattero.com>
2021-05-15 22:16:45 +0200

    auto bed leveling fixed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1309,9 +1309,9 @@
  */
 //#define AUTO_BED_LEVELING_3POINT
 //#define AUTO_BED_LEVELING_LINEAR
-//#define AUTO_BED_LEVELING_BILINEAR
+#define AUTO_BED_LEVELING_BILINEAR
 //#define AUTO_BED_LEVELING_UBL
-#define MESH_BED_LEVELING
+// #define MESH_BED_LEVELING
 
 /**
  * Normally G28 leaves leveling disabled on completion. Enable one of
