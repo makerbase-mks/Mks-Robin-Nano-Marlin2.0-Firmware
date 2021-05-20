41d375d11574ce0226aceb7a2af1e834685badba
Davide Zavattero <davide@davidezavattero.com>
2021-05-21 01:40:04 +0200

    direzione estrusore

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1127,7 +1127,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define  INVERT_E0_DIR true   // false
+#define  INVERT_E0_DIR false   // false
 #define  INVERT_E1_DIR false   // false
 #define  INVERT_E2_DIR false   // false
 #define  INVERT_E3_DIR false   // false
