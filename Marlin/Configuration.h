7a09f9e33530edee78f429c816562bbd2a3765ed
Davide Zavattero <davide@davidezavattero.com>
2021-08-08 15:15:21 +0200

    test

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1318,8 +1318,8 @@
  * these options to restore the prior leveling state or to always enable
  * leveling immediately after G28.
  */
-#define RESTORE_LEVELING_AFTER_G28
-//#define ENABLE_LEVELING_AFTER_G28
+// #define RESTORE_LEVELING_AFTER_G28
+#define ENABLE_LEVELING_AFTER_G28
 
 /**
  * Auto-leveling needs preheating
