8c7d33d99765266f337782242bb481ba0a686811
Davide Zavattero <davide@davidezavattero.com>
2021-08-08 10:57:48 +0200

    Revert custom configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -673,15 +673,15 @@
  *          TMC5130, TMC5130_STANDALONE, TMC5160, TMC5160_STANDALONE
  * :['A4988', 'A5984', 'DRV8825', 'LV8729', 'L6470', 'L6474', 'POWERSTEP01', 'TB6560', 'TB6600', 'TMC2100', 'TMC2130', 'TMC2130_STANDALONE', 'TMC2160', 'TMC2160_STANDALONE', 'TMC2208', 'TMC2208_STANDALONE', 'TMC2209', 'TMC2209_STANDALONE', 'TMC26X', 'TMC26X_STANDALONE', 'TMC2660', 'TMC2660_STANDALONE', 'TMC5130', 'TMC5130_STANDALONE', 'TMC5160', 'TMC5160_STANDALONE']
  */
-#define  X_DRIVER_TYPE TMC2209_STANDALONE   //  A4988
-#define  Y_DRIVER_TYPE TMC2209_STANDALONE   //  A4988
-#define  Z_DRIVER_TYPE TMC2208_STANDALONE   //  A4988
+#define  X_DRIVER_TYPE TMC2208_STANDALONE   //  A4988
+#define  Y_DRIVER_TYPE TMC2208_STANDALONE   //  A4988
+#define  Z_DRIVER_TYPE A4988   //  A4988
 //#define  X2_DRIVER_TYPE A4988   // A4988
 //#define  Y2_DRIVER_TYPE A4988   // A4988
 //#define  Z2_DRIVER_TYPE A4988   // A4988
 //#define  Z3_DRIVER_TYPE A4988   // A4988
 //#define  Z4_DRIVER_TYPE A4988   // A4988
-#define  E0_DRIVER_TYPE TMC2209_STANDALONE   // A4988
+#define  E0_DRIVER_TYPE A4988   // A4988
 //#define  E1_DRIVER_TYPE A4988   // A4988
 //#define  E2_DRIVER_TYPE A4988   // A4988
 //#define  E3_DRIVER_TYPE A4988   // A4988
@@ -736,7 +736,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 399, 408}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 419}
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1122,12 +1122,12 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define  INVERT_X_DIR false   // false
 #define  INVERT_Y_DIR false   // true
-#define  INVERT_Z_DIR true   // false
+#define  INVERT_Z_DIR false   // false
 
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define  INVERT_E0_DIR true   // false
+#define  INVERT_E0_DIR false   // false
 #define  INVERT_E1_DIR false   // false
 #define  INVERT_E2_DIR false   // false
 #define  INVERT_E3_DIR false   // false
