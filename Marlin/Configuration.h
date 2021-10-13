41ec9a89c799bfc4d3c6cbd59c408f5ff22b7e2f
Davide Zavattero <davide@davidezavattero.com>
2021-10-13 02:14:19 +0200

    fix per stampante

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -744,7 +744,7 @@
 //#define  Z2_DRIVER_TYPE A4988   // A4988
 //#define  Z3_DRIVER_TYPE A4988   // A4988
 //#define  Z4_DRIVER_TYPE A4988   // A4988
-#define  E0_DRIVER_TYPE A4988   // A4988
+#define  E0_DRIVER_TYPE TMC2209_STANDALONE   // A4988
 //#define  E1_DRIVER_TYPE A4988   // A4988
 //#define  E2_DRIVER_TYPE A4988   // A4988
 //#define  E3_DRIVER_TYPE A4988   // A4988
@@ -799,7 +799,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 394.9}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 825.423}
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1190,7 +1190,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
+#define INVERT_E0_DIR true
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
