a691e56fbb2026f73925507d03bc31420eb3a049
Davide Zavattero <davide@davidezavattero.com>
2021-10-23 01:08:33 +0200

    fix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1322,7 +1322,7 @@
   // After a runout is detected, continue printing this length of filament
   // before executing the runout script. Useful for a sensor at the end of
   // a feed tube. Requires 4 bytes SRAM per sensor, plus 4 bytes overhead.
-  // #define FILAMENT_RUNOUT_DISTANCE_MM 500
+  #define FILAMENT_RUNOUT_DISTANCE_MM 500
 
   #ifdef FILAMENT_RUNOUT_DISTANCE_MM
     // Enable this option to use an encoder disc that toggles the runout pin
