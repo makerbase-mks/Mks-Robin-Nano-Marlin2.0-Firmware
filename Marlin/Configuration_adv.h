ea2a1f0e79ac33bae7d90485c56edefac8e7eec1
Davide Zavattero <davide@davidezavattero.com>
2021-10-23 01:07:45 +0200

    fix buffer

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2094,7 +2094,7 @@
 
 // The ASCII buffer for serial input
 #define MAX_CMD_SIZE 96
-#define BUFSIZE 16
+#define BUFSIZE 4
 
 // Transmission to Host Buffer Size
 // To save 386 bytes of PROGMEM (and TX_BUFFER_SIZE+3 bytes of RAM) set to 0.
