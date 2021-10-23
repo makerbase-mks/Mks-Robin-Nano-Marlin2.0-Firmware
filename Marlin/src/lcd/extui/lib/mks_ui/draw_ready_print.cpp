f5b0530eb44d45731bcc45549d5d622c38dca7cd
Davide Zavattero <davide@davidezavattero.com>
2021-10-24 01:36:04 +0200

    preheating labels

--- Marlin/src/lcd/extui/lib/mks_ui/draw_ready_print.cpp
+++ Marlin/src/lcd/extui/lib/mks_ui/draw_ready_print.cpp
@@ -47,13 +47,13 @@ static lv_obj_t *scr;
 static lv_obj_t *labelExt1;
 
 
-static lv_obj_t *btn_pla;
-static lv_obj_t *btn_abs;
+
 static lv_obj_t *label_abs;
 static lv_obj_t *label_pla;
 
-// static lv_style_t btn_style_pre;
-// static lv_style_t btn_style_rel;
+static lv_style_t btn_style_pre;
+static lv_style_t btn_style_rel;
+static int additional;
 
 #if HAS_MULTI_EXTRUDER && DISABLED(SINGLENOZZLE)
   static lv_obj_t *labelExt2;
@@ -237,9 +237,9 @@ void lv_draw_ready_print(void) {
   }
   else {
     scr = lv_screen_create(PRINT_READY_UI, "");
-    lv_big_button_create(scr, "F:/bmp_tool.bin", main_menu.tool, 20, 90, event_handler, ID_TOOL);
-    lv_big_button_create(scr, "F:/bmp_set.bin", main_menu.set, 180, 90, event_handler, ID_SET);
-    lv_big_button_create(scr, "F:/bmp_printing.bin", main_menu.print, 340, 90, event_handler, ID_PRINT);
+    lv_big_button_create(scr, "F:/bmp_tool.bin", main_menu.tool, 20, 60, event_handler, ID_TOOL);
+    lv_big_button_create(scr, "F:/bmp_set.bin", main_menu.set, 180, 60, event_handler, ID_SET);
+    lv_big_button_create(scr, "F:/bmp_printing.bin", main_menu.print, 340, 60, event_handler, ID_PRINT);
     lv_temp_info();
     disp_ext_heart_ready_print();
   }
@@ -258,20 +258,20 @@ void lv_temp_info() {
   // Create image buttons
 
   #if HAS_HEATED_BED
-    lv_big_button_create(scr, "F:/bmp_bed_state.bin", " ", 180, 260, event_handler, ID_INFO_BED);
+    lv_big_button_create(scr, "F:/bmp_bed_state.bin", " ", ( 20), 210, event_handler, ID_INFO_BED);
   #endif
 
-  lv_big_button_create(scr, "F:/bmp_ext1_state.bin", " ", 340, 260, event_handler, ID_INFO_EXT);
+  lv_big_button_create(scr, "F:/bmp_ext1_state.bin", " ", ( 20), 260, event_handler, ID_INFO_EXT);
 
   #if HAS_MULTI_EXTRUDER && DISABLED(SINGLENOZZLE)
     lv_big_button_create(scr, "F:/bmp_ext2_state.bin", " ", 325, 260, event_handler, ID_INFO_EXT);
   #endif
 
   #if HAS_HEATED_BED
-    labelBed = lv_label_create(scr, (180 + 50), 270, nullptr);
+    labelBed = lv_label_create(scr, ( 20 + 50), 220, nullptr);
     #endif
 
-    labelExt1 = lv_label_create(scr, (340 + 50), 270, nullptr);
+    labelExt1 = lv_label_create(scr, ( 20 + 50), 270, nullptr);
 
     #if HAS_MULTI_EXTRUDER && DISABLED(SINGLENOZZLE)
       labelExt2 = lv_label_create(scr, 375, 270, nullptr);
@@ -304,12 +304,29 @@ void lv_clear_ready_print() {
 }
 
 void disp_ext_heart_ready_print() {
+    lv_big_button_create(scr, "F:/bmp_temp_mini.bin"," ", ( 180), 210, event_handler, ID_P_PLA);
+    lv_big_button_create(scr, "F:/bmp_temp_mini.bin"," ", ( 180), 260, event_handler, ID_P_ABS);
 
-    btn_pla = lv_imgbtn_create(scr, "F:/bmp_temp_mini.bin", 20, 130, event_handler, ID_P_PLA);
-    btn_abs = lv_imgbtn_create(scr, "F:/bmp_temp_mini.bin", 20, 180,  event_handler, ID_P_ABS);
+    label_pla = lv_label_create(scr, (180+50), (210 + 10), PREHEAT_1_LABEL);
+    label_abs = lv_label_create(scr, (180+50), (260 + 10), PREHEAT_2_LABEL);
+    // label_pla = lv_btn_create(scr, ( 180+45), (210), 80, 40, event_handler, ID_P_PLA);
+    // label_abs = lv_btn_create(scr, ( 180+45), (260), 80, 40, event_handler, ID_P_ABS);
 
-    label_pla = lv_label_create(btn_pla, 50, 10, PREHEAT_1_LABEL);
-    label_abs = lv_label_create(btn_abs, 50, 10, PREHEAT_2_LABEL);
+    // lv_label_create(label_pla,PREHEAT_1_LABEL);
+    // lv_label_create(label_abs,PREHEAT_2_LABEL);
+
+    // lv_btn_set_style(label_pla, LV_BTN_STYLE_PR, &btn_style_pre);
+    // lv_btn_set_style(label_pla, LV_BTN_STYLE_REL, &btn_style_rel);
+    // lv_label_set_long_mode(label_pla, LV_LABEL_LONG_EXPAND);
+    // lv_label_set_align(label_pla,LV_LABEL_ALIGN_LEFT);
+
+
+    // lv_btn_set_layout(label_abs,LV_LABEL_ALIGN_LEFT);
+
+    // lv_btn_set_style(label_abs, LV_BTN_STYLE_PR, &btn_style_pre);
+    // lv_btn_set_style(label_abs, LV_BTN_STYLE_REL, &btn_style_rel);
+    // lv_label_set_long_mode(label_abs, LV_LABEL_LONG_EXPAND);
+    // lv_label_set_align(label_abs,LV_LABEL_ALIGN_LEFT);
 
 
 }
