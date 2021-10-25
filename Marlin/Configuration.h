eec6f22463e82402f48a6f2a6a5cbf0d35a592ad
MORFEUS <84046448+ItalyMakers@users.noreply.github.com>
2021-10-25 11:43:15 +0200

    Update Configuration.h
    cambio temperatura per il piatto

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1688,7 +1688,7 @@
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
 #define PREHEAT_1_TEMP_HOTEND 200
-#define PREHEAT_1_TEMP_BED     55
+#define PREHEAT_1_TEMP_BED     65
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "PETG"
