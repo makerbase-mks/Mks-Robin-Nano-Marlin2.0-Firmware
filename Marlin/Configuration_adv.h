947e4669fe4f08e6dbfca3e18d961e6660387541
Davide Zavattero <davide@davidezavattero.com>
2021-10-25 22:51:16 +0200

    fix

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2083,11 +2083,11 @@
 // The number of linear moves that can be in the planner at once.
 // The value of BLOCK_BUFFER_SIZE must be a power of 2 (e.g. 8, 16, 32)
 #if BOTH(SDSUPPORT, DIRECT_STEPPING)
-  #define  BLOCK_BUFFER_SIZE 16   //  8
+  #define  BLOCK_BUFFER_SIZE 32   //  8
 #elif ENABLED(SDSUPPORT)
-  #define BLOCK_BUFFER_SIZE 16
+  #define BLOCK_BUFFER_SIZE 32
 #else
-  #define BLOCK_BUFFER_SIZE 16
+  #define BLOCK_BUFFER_SIZE 32
 #endif
 
 // @section serial
