6dd2facc8950e1365786ebd9f40a5dabd7e9b449
Davide Zavattero <davide@davidezavattero.com>
2021-10-26 23:02:42 +0200

    customization

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -860,7 +860,7 @@
   #endif
 #endif
 
-#define DEFAULT_EJERK    2.0  // May be used by Linear Advance
+#define DEFAULT_EJERK    10.0  // May be used by Linear Advance
 
 /**
  * Junction Deviation Factor
@@ -1317,7 +1317,7 @@
 
   // Set one or more commands to execute on filament runout.
   // (After 'M412 H' Marlin will ask the host to handle the process.)
-  #define FILAMENT_RUNOUT_SCRIPT "M25"
+  #define FILAMENT_RUNOUT_SCRIPT "M600"
 
   // After a runout is detected, continue printing this length of filament
   // before executing the runout script. Useful for a sensor at the end of
