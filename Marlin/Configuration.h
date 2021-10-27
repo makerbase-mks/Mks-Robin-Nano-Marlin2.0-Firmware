286c99911bde4e70ae8545b38256ca25f7cbdf08
MORFEUS <84046448+ItalyMakers@users.noreply.github.com>
2021-10-27 08:46:16 +0200

    Update Configuration.h
    correzione step mm estrusore

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -799,7 +799,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 419}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 405}
 
 /**
  * Default Max Feed Rate (mm/s)
